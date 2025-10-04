77c628b85189771cef496a16fdbf31f7f6d5de57
AmanJ4588 <amanj4588@gmail.com>
2025-10-04 19:19:16 +0530

    Modified Project Structure for better organization

--- data_preprocessing/02_load_to_duckdb/answers_to_duckDB.py
+++ data_preprocessing/02_load_to_duckdb/answers_to_duckDB.py
@@ -2,7 +2,7 @@ import duckdb
 con = duckdb.connect("dataset/cert42.duckdb") 
 
 #only insiders.csv will go in as scenario based TP user logs are already present in the og log files
-file_path = "dataset/answers/insiders.csv"
+file_path = "../../dataset/answers/insiders.csv"
 
 
 sql_query = """CREATE TABLE insiders AS 
